06015f0464c503ac61cf33b4ff045e3d8c839d01
mziz <milda_zizyte@brown.edu>
2024-02-06 14:13:03 +0000

    make step 8 the default for single stage cpu

--- src/processors/CS1952y/single_stage_cpu/cs1952y1s_cpu.h
+++ src/processors/CS1952y/single_stage_cpu/cs1952y1s_cpu.h
@@ -13,6 +13,8 @@
 #include "../../RISC-V/rv_registerfile.h"
 
 #include "cs1952y_alu.h"
+#include "cs1952y1s_control.h"
+#include "cs1952y1s_decode.h"
 #include "cs1952y1s_enums.h"
 
 namespace vsrtl {
@@ -31,48 +33,112 @@ public:
     m_enabledISA = std::make_shared<ISAInfo<XLenToRVISA<XLEN>()>>(extensions);    
     
     // ** ADVANCING THE PC **
-    pc_inc->out >> pc_reg->in;
+    pc_sel->out >> pc_reg->in;
     pc_reg->out >> pc_inc->op1;
     4 >> pc_inc->op2;
+
+    // Jumps and branches
+    registers->r1_out >> pc_add1->get(PCAdd1::RS); // JALR
+    pc_reg->out >> pc_add1->get(PCAdd1::PC); // JAL
+    control->pc_add1 >> pc_add1->select;
+    pc_add1->out >> pc_add->op1;
+    imm_sel->out >> pc_add->op2;
+    pc_inc->out >> pc_sel->get(PCSel::INC);
+    pc_add->out >> pc_sel->get(PCSel::JB);
+
+    j_or_b->out >> pc_sel->select;
+    control->jump >> *j_or_b->in[0];
+    branch->out >> *j_or_b->in[1];
+    control->branch >> *branch->in[0];
+    inv_zero->out >> *branch->in[1];
+    alu->zero >> *inv_zero->in[0];
+    control->inv_zero >> *inv_zero->in[1];
+
 
     // ** Instruction memory **
     instr_mem->setMemory(m_memory);
     pc_reg->out >> instr_mem->addr;
 
+    // ** Decode and Control **
+    instr_mem->data_out >> decode->instr;
+    decode->opcode >> control->opcode;
+    decode->funct3 >> control->funct3;
+    decode->funct7 >> control->funct7;
+
+    // Immediates
+    decode->imm_I >> imm_sel->get(ImmSel::I);
+    decode->imm_Ishift >>  imm_sel->get(ImmSel::Ishift);
+    decode->imm_U >> imm_sel->get(ImmSel::U);
+    decode->imm_S >> imm_sel->get(ImmSel::S);
+    decode->imm_J >> imm_sel->get(ImmSel::J);
+    decode->imm_B >> imm_sel->get(ImmSel::B);
+    control->imm_sel >> imm_sel->select;
+
     // ** Registers **
     registers->setMemory(m_regMem);
-    0 >> registers->r1_addr;
-    0 >> registers->r2_addr;
-    0 >> registers->wr_addr;
-    0 >> registers->data_in;
-    0 >> registers->wr_en;
+    decode->rs1 >> registers->r1_addr;
+    decode->rs2 >> registers->r2_addr;
+
+    // Writeback
+    decode->rd1 >> registers->wr_addr;
+    control->reg_w >> registers->wr_en;
+    alu->res >> rd_sel->get(RdSel::ALU);
+    imm_sel->out >> rd_sel->get(RdSel::IMM);
+    data_mem->data_out >> rd_sel->get(RdSel::MEM);
+    pc_inc->out >> rd_sel->get(RdSel::PC_L);
+    control->rd_sel >> rd_sel->select;
+    rd_sel->out >> registers->data_in;
     
     // ** ALU **
-    0 >> alu->op1;
-    0 >> alu->op2;
-    ALUOp::NOP >> alu->ctrl;
+    // Select op1
+    registers->r1_out >> alu_op1_sel->get(ALU1Sel::REG1);
+    pc_reg->out >> alu_op1_sel->get(ALU1Sel::PC);
+    control->alu1_sel >> alu_op1_sel->select;
+    // Select op2
+    registers->r2_out >> alu_op2_sel->get(ALU2Sel::REG2);
+    imm_sel->out >> alu_op2_sel->get(ALU2Sel::IMM);
+    control->alu2_sel >> alu_op2_sel->select;
+
+    alu_op1_sel->out >> alu->op1;
+    alu_op2_sel->out >> alu->op2;
+    control->alu_ctrl >> alu->ctrl;
     
     // Data memory
     data_mem->mem->setMemory(m_memory);
-    0 >> data_mem->addr;
-    0 >> data_mem -> data_in;
-    0 >> data_mem->wr_en;
-    MemOp::NOP >> data_mem->op;
+    alu->res >> data_mem->addr;
+    registers->r2_out >> data_mem -> data_in;
+    control->mem_w >> data_mem->wr_en;
+    control->mem_op >> data_mem->op;
     
-    RVInstr::NOP >> ecallChecker->opcode;
+    decode->ecall >> ecallChecker->opcode;
     ecallChecker->setSyscallCallback(&trapHandler);
     0 >> ecallChecker->stallEcallHandling;
   }
 
   // PC
   SUBCOMPONENT(pc_reg, Register<XLEN>);
-  SUBCOMPONENT(pc_inc, Adder<XLEN>); // ** ADVANCING THE PC **
-  
-  // ALU
-  SUBCOMPONENT(alu, TYPE(CS1952yALU<XLEN>));
+  SUBCOMPONENT(pc_inc, Adder<XLEN>);
+  SUBCOMPONENT(pc_add, Adder<XLEN>);
+  SUBCOMPONENT(pc_add1, TYPE(EnumMultiplexer<PCAdd1, XLEN>));
+  SUBCOMPONENT(pc_sel, TYPE(EnumMultiplexer<PCSel, XLEN>));
+
+  SUBCOMPONENT(j_or_b, TYPE(Or<1, 2>));
+  SUBCOMPONENT(branch, TYPE(And<1, 2>));
+  SUBCOMPONENT(inv_zero, TYPE(Xor<1, 2>));
+
+  // Decode and Control
+  SUBCOMPONENT(decode,  CS1952y1sDecode<XLEN>);
+  SUBCOMPONENT(control, CS1952y1sControl<XLEN>);
+  SUBCOMPONENT(imm_sel, TYPE(EnumMultiplexer<ImmSel, XLEN>));
   
   // Register File
   SUBCOMPONENT(registers, TYPE(RegisterFile<XLEN, false>));
+  SUBCOMPONENT(rd_sel, TYPE(EnumMultiplexer<RdSel, XLEN>));
+
+  // ALU
+  SUBCOMPONENT(alu_op1_sel, TYPE(EnumMultiplexer<ALU1Sel, XLEN>));
+  SUBCOMPONENT(alu_op2_sel, TYPE(EnumMultiplexer<ALU2Sel, XLEN>));
+  SUBCOMPONENT(alu, TYPE(CS1952yALU<XLEN>));
   
   // Address spaces
   ADDRESSSPACEMM(m_memory);
@@ -89,7 +155,7 @@ public:
   unsigned int getPcForStage(StageIndex) const override {
     return pc_reg->out.uValue();
   }
-  AInt nextFetchedAddress() const override { return pc_inc->out.uValue(); }
+  AInt nextFetchedAddress() const override { return pc_sel->out.uValue(); }
   QString stageName(StageIndex) const override { return "•"; }
   StageInfo stageInfo(StageIndex) const override {
     return StageInfo({pc_reg->out.uValue(),
--- src/processors/CS1952y/single_stage_cpu/cs1952y1s_enums.h
+++ src/processors/CS1952y/single_stage_cpu/cs1952y1s_enums.h
@@ -5,6 +5,11 @@
 namespace vsrtl {
 namespace core {
 using namespace Ripes;
-
+    Enum(ALU1Sel, REG1, PC);
+    Enum(ALU2Sel, REG2, IMM);
+    Enum(ImmSel, I, Ishift, U, S, J, B);
+    Enum(RdSel, ALU, IMM, MEM, PC_L);
+    Enum(PCAdd1, PC, RS);
+    Enum(PCSel, INC = 0, JB = 1);
 }
 }
